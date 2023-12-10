f5030b583a2e3064cb1497d0d0128b29568b7ee6
liu12 <8981024@qq.com>
2023-12-10 20:54:31 +0800

    最后处理 www

--- 4.8.5-7.4/start.sh
+++ 4.8.5-7.4/start.sh
@@ -1,31 +1,6 @@
 #!/bin/sh
 
 
-if [ ! -d /data/www ]; then
-	mkdir -p /data/www
-fi
-
-
-if [ ! "$(ls -A /data/www)" ]; then
-	# 代码目录为空，创建项目
-	cd /root/
-	composer create-project be/new
-	cp -rf new/* /data/www
-	rm -rf new
-		
-	if [ ! -d /data/www/data ]; then
-		mkdir -p /data/www/data
-		chmod -R 777 /data/www/data
-	fi
-	
-	if [ ! -d /data/www/www ]; then
-		mkdir -p /data/www/data
-		chmod -R 777 /data/www/www
-	fi
-fi
-
-
-
 if [ ${MYSQL-SERVER} ]; then
 	# 安装本机 MySQL
 	echo '安装本机 MySQL'
@@ -39,17 +14,13 @@ if [ ${MYSQL-SERVER} ]; then
 	if [ ! -d /data/mysql/conf ]; then
 		mkdir -p /data/mysql/conf
 	fi
-	
 fi
 
 
-
 if [ ${REDIS-SERVER} ]; then
-
 	if [ ! -f /etc/init.d/redis-server ]; then
-	
-		echo '安装本机 REDIS'
 		# 安装本机 REDIS
+		echo '安装本机 REDIS'
 		apt-get update
 		apt-get install -y lsb-release curl gpg
 		curl -fsSL https://packages.redis.io/gpg | gpg --dearmor -o /usr/share/keyrings/redis-archive-keyring.gpg
@@ -74,6 +45,28 @@ if [ ${REDIS-SERVER} ]; then
 	/etc/init.d/redis-server /data/redis/conf/redis.conf --daemonize yes
 fi
 
+
+if [ ! -d /data/www ]; then
+	mkdir -p /data/www
+fi
+
+if [ ! "$(ls -A /data/www)" ]; then
+	# 代码目录为空，创建项目
+	cd /root/
+	composer create-project be/new
+	cp -rf new/* /data/www
+	rm -rf new
+		
+	if [ ! -d /data/www/data ]; then
+		mkdir -p /data/www/data
+		chmod -R 777 /data/www/data
+	fi
+	
+	if [ ! -d /data/www/www ]; then
+		mkdir -p /data/www/data
+		chmod -R 777 /data/www/www
+	fi
+fi
 
 cd /data/www
 php server.php start
